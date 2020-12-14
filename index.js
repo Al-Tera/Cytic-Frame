efccadea0e1e2d57272ac9668ce5e30de0ad7791
Altair <lake.horta@gmail.com>
2020-12-14 15:27:13 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,8 @@
+var background = document.getElementById("bgtop")
+for (i = 0; i < 120; i++){
+background.innerHTML += "<div class='box'></div>"
+}
+var background2 = document.getElementById("bgbottom")
+for (i = 0; i < 120; i++){
+background2.innerHTML += "<div class='box'></div>"
+}
